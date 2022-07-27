d7ed554c5977d3c88fb7766c7115446dea34333f
MARCELO SANCHES OLIVEIRA <22102392@dsn127m1246726.cfp127aluno.local>
2022-07-27 11:26:22 -0300

    aula01

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function juntarNome () {
+
+    const nome = document.getElementById('nome').value
+    const sobrenome = document.getElementById('sobrenome').value
+    const resultado = document.getElementById('resultado')
+
+    const nomeCompleto = `Seu nome completo é: ${nome} ${sobrenome}`
+    resultado.textContent = nomeCompleto
+}
+
+document.getElementById('juntar').addEventListener('click',juntarNome)
+
